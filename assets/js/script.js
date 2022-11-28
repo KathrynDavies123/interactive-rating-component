4500d07c29968f71c1b9033505b678d764fbfd87
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2022-11-28 13:22:38 +0100

    finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
 let submitbutton = document.querySelector(".submitbutton");
 
-let ratingbutton = document.getElementsByClassName("ratingbutton");
+let ratingbutton = document.querySelectorAll(".ratingbutton");
 
-for (let i = 0; i < ratingbutton.length; i++) {
-    ratingbutton[i].addEventListener("click", function (event) {
-      event.preventDefault();
-      ratingbutton[i].classList.toggle("active");
-      let selectedRating = ratingbutton[i].innerText;
-      document.querySelector(
-        ".ratingselection"
-      ).innerText = `You selected ${selectedRating} out of 5`;
-    });
-  }
+ratingbutton.forEach(function (item) {
+  item.addEventListener("click", function (event) {
+    event.preventDefault();
+    ratingbutton.forEach(function(item) {
+      item.classList.remove("active");
+    })
+    item.classList.add("active");
+    let selectedRating = item.innerText;
+    document.querySelector(".ratingselection").innerText = `You selected ${selectedRating} out of 5`;
+  });
+});
 
 submitbutton.addEventListener("click", function (event) {
   event.preventDefault();
   document.querySelector(".ratingcomponent").style.display = "none";
-  document.querySelector(".thankyoucomponent").style.display = "block";
+  document.querySelector(".thankyoucomponent").style.display = "flex";
 });
