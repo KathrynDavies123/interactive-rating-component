6540cd50f890ec617c2bcaab6dcd9f5c7cd1deb4
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2022-11-28 17:56:06 +0100

    fixed submit button to only work when a number is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,18 @@ let ratingbutton = document.querySelectorAll(".ratingbutton");
 ratingbutton.forEach(function (item) {
   item.addEventListener("click", function (event) {
     event.preventDefault();
-    ratingbutton.forEach(function(item) {
+    ratingbutton.forEach(function (item) {
       item.classList.remove("active");
-    })
+    });
     item.classList.add("active");
     let selectedRating = item.innerText;
-    document.querySelector(".ratingselection").innerText = `You selected ${selectedRating} out of 5`;
+    document.querySelector(
+      ".ratingselection"
+    ).innerText = `You selected ${selectedRating} out of 5`;
+    submitbutton.addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector(".ratingcomponent").style.display = "none";
+      document.querySelector(".thankyoucomponent").style.display = "flex";
+    });
   });
-});
-
-submitbutton.addEventListener("click", function (event) {
-  event.preventDefault();
-  document.querySelector(".ratingcomponent").style.display = "none";
-  document.querySelector(".thankyoucomponent").style.display = "flex";
 });
